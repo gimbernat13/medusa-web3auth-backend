495146d8522451e687838cbe8799dcef6555d388
Diego Gimbernat <gimbernat13@gmail.com>
2023-06-13 12:19:26 -0500

    🔨Added Nonce endpoint

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,55 @@
-import { Router } from "express"
+import { Router } from "express";
+import * as dotenv from "dotenv";
+import * as jwt from "jsonwebtoken"; //TODO: Import only necessary parts of jwt
+
+dotenv.config();
 
 export default (rootDirectory: string): Router | Router[] => {
-  // add your custom routes here
-  return []
-}
+  const router = Router();
+  const jwtSecret = process.env.JWT_SECRET;
+
+  // Define your custom route
+  router.get("/nonce", (req, res) => {
+    const nonce = new Date().getTime();
+    const address = req.query.address;
+
+    const tempToken = jwt.sign({ nonce, address }, jwtSecret, {
+      expiresIn: "60s",
+    });
+
+    const message = getSignMessage(address, nonce);
+    // const message = "Pls sign message ";
+
+    res.json({ tempToken, message });
+  });
+
+  router.post("/verify", async (req, res) => {
+    const authHeader = req.headers["authorization"];
+    const tempToken = authHeader && authHeader.split(" ")[1];
+
+    if (tempToken === null) return res.sendStatus(403);
+
+    const userData = await jwt.verify(tempToken, jwtSecret);
+    const nonce = userData.nonce;
+    const address = userData.address;
+    const message = getSignMessage(address, nonce);
+    const signature = req.query.signature;
+
+    // const verifiedAddress = await web3.eth.accounts.recover(message, signature);
+    const verifiedAddress = "mierda";
+    if (verifiedAddress.toLowerCase() == address.toLowerCase()) {
+      const token = jwt.sign({ verifiedAddress }, jwtSecret, {
+        expiresIn: "1d",
+      });
+      res.json({ token });
+    } else {
+      res.sendStatus(403);
+    }
+  });
+
+  const getSignMessage = (address, nonce) => {
+    return `Please sign this message for address ${address}:\n\n${nonce}`;
+  };
+
+  return router;
+};
